57b509327bcf896584e6d2d88206af772587eddf
haseong <kimhaseong@gmail.com>
2021-03-19 21:24:56 +0900

    update

--- rocker-rstudio/docker_run.sh
+++ rocker-rstudio/docker_run.sh
@@ -3,6 +3,7 @@ sudo docker run -d --rm \
 	-p 8787:8787 \
 	-e PASSWORD=haseong \
 	-v /home/haseong/alpha/:/alpha/ \
+	-v /home/haseong/alpha/dev/:/home/rstudio/alpha/dev/ \
 	--name rstudio-server \
 	haseong/rocker-rstudio:v0.4
 	#haseong/rstudio-server-run:v02
