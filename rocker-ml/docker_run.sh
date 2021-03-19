c742765a42b6db1c7f78d6dc8db660ad55fc8544
haseong <kimhaseong@gmail.com>
2021-03-19 21:22:39 +0900

    update

--- rocker-ml/docker_run.sh
+++ rocker-ml/docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker run -d --rm -v /home/haseong/alpha:/home/rstudio/alpha --name rstudio -e PASSWORD=haseong -p 8787:8787 haseong/rocker-ml:v4.4
+sudo docker run -d --rm -v /home/haseong/share:/home/rstudio/share -v /home/haseong/alpha:/home/rstudio/alpha --name rstudio -e USER=haseong -p 8787:8787 haseong/rocker-ml:v4.5
 
