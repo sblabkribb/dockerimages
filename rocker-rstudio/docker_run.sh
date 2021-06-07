a69f4de6e21ec4ce599f70e26dcef47d1dabd6dd
haseong <kimhaseong@gmail.com>
2021-06-07 21:18:48 +0900

    update

--- rocker-rstudio/docker_run.sh
+++ rocker-rstudio/docker_run.sh
@@ -2,7 +2,9 @@
 sudo docker run -d --rm \
 	-p 8787:8787 \
 	-v /home/haseong/:/home/rstudio/ \
+	-v /mnt/153_share/:/home/rstudio/153_share \
+	-v /mnt/153_SeqDB/:/home/rstudio/153_SeqDB \
 	--name rstudio-server \
-	haseong/rocker-rstudio:v0.7
+	haseong/rocker-rstudio:v0.8
 
 
