2b7e4716382b6e86f34e65f4a2c625c39938deed
sblabkribb <haseong@kribb.re.kr>
2021-06-30 12:23:25 +0900

    update

--- rocker-rstudio/docker_build.sh
+++ rocker-rstudio/docker_build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-#docker build --no-cache=true -t haseong/rocker-rstudio:v1.1 .
-docker build -t haseong/rocker-rstudio:v1.1 .
-=======
-docker build --no-cache=true -t haseong/rocker-rstudio:v1.1 .
-#docker build -t haseong/rocker-rstudio:v1.0 .
->>>>>>> 03daa72c70c61b46be5ad7b60b310f36aa076c12
+docker build --no-cache=true -t haseong/rocker-rstudio:v1.2 .
+#docker build -t haseong/rocker-rstudio:v1.1 .
 
