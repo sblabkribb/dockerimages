17d69eabcc8ecca50c98ab5200da3ce3f3f54434
haseong <kimhaseong@gmail.com>
2021-08-20 12:11:56 +0900

    update

--- mysql/docker_run.bat
+++ mysql/docker_run.bat
@@ -1,1 +1,1 @@
-docker run --rm  -p 3306:3306  --name mysqldock -v DBfiles:/var/lib/mysql -e MYSQL_ROOT_PASSWORD="rootpass" -e MYSQL_USER="haseong" -e MYSQL_PASSWORD="haseongpass" haseong/mysql:v0 
+docker run --rm -d  -p 3306:3306  --name mysql -v d:\mydocs\mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD="rootpass" -e MYSQL_USER="haseong" -e MYSQL_PASSWORD="haseongpass" mysql:latest 
