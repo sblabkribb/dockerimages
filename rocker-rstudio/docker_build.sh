f5df337df34e0ad8fd4bde265d14452f47f8c92c
haseong <haseong@kribb.re.kr>
2022-03-14 12:54:52 +0900

    update rocker-rstudio

--- rocker-rstudio/docker_build.sh
+++ rocker-rstudio/docker_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#docker build --no-cache=true -t haseong/rocker-rstudio:v1.5 .
-docker build -t haseong/rocker-rstudio:v1.8 .
+#docker build --no-cache=true -t haseong/rocker-rstudio:v1.9 .
+docker build -t haseong/rocker-rstudio:v1.9 .
 
