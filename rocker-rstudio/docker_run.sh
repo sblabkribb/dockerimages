dc3286eacb2c67f6553bc6c3bab63faa26e4b4b4
haseong <haseong@kribb.re.kr>
2022-06-13 10:13:08 +0900

    update

--- rocker-rstudio/docker_run.sh
+++ rocker-rstudio/docker_run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 docker run -d --rm \
 	-v /mnt/alpha:/mnt/alpha \
+	-v /etc/localtime:/etc/localtime \
 	-p 8787:8787 \
 	--name rstudio \
 	haseong/rocker-rstudio:v2.1
+
+docker network connect sblab rstudio 
 
 sudo bash /mnt/alpha/dockerimages/bin/rstudio_users_sync.sh rstudio
