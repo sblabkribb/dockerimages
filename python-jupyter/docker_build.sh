294c3ae8c51643f5a0a513d5a594f70fdda24dda
haseong <haseong@kribb.re.kr>
2022-09-22 19:19:24 +0900

    update

--- python-jupyter/docker_build.sh
+++ python-jupyter/docker_build.sh
@@ -1,2 +1,2 @@
-#docker build --no-cache -t haseong/python-jupyter:v0.2 .
-docker build -t haseong/python-jupyter:v0.2 .
+docker build --no-cache -t haseong/python-jupyter:v0.3 .
+#docker build -t haseong/python-jupyter:v0.3 .
