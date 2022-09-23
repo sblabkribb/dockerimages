1647fb8a83acf42f42d321b80c2d861849206733
haseong <haseong@kribb.re.kr>
2022-09-23 18:50:59 +0900

    update

--- mysql/docker_run.sh
+++ mysql/docker_run.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 docker pull mysql:8.0
-docker run --rm -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=mysqlpass  --name mysql -v /mnt/alpha/mysql:/var/lib/mysql mysql:8.0
+docker run --rm -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=hello --name mysql \
+	   -v /mnt/alpha/mysql/:/var/lib/mysql \
+	   -v /mnt/alpha/dockerimages/mysql/:/docker-entrypoint-initdb.d \
+	   mysql:8.0
