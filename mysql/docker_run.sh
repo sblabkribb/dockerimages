7e897bfd3778c48dd1794e6d79bedfc5ee95c955
haseong <haseong@kribb.re.kr>
2022-10-28 17:54:00 +0900

    update

--- mysql/docker_run.sh
+++ mysql/docker_run.sh
@@ -2,4 +2,4 @@
 #docker pull mysql:8.0
 VER=8.0
 docker pull mysql:$VER
-docker run --rm -d -p 3306:3306 --name mysql -e MYSQL_RANDOM_ROOT_PASSWORD=yes -v /mnt/alpha/mysql_20220503:/var/lib/mysql mysql:$VER
+docker run --rm -d -p 3306:3306 --name mysql -e MYSQL_RANDOM_ROOT_PASSWORD=yes -v /mnt/alpha/mysql:/var/lib/mysql mysql:$VER
