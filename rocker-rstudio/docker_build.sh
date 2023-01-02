8d41859357473a08ea5969108a40e8bf6f837d02
haseong <haseong@kribb.re.kr>
2023-01-03 06:17:39 +0900

    update rocker-rstudio

--- rocker-rstudio/docker_build.sh
+++ rocker-rstudio/docker_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #docker build --no-cache=true -t haseong/rocker-rstudio:v2.2 .
-docker build -t haseong/rocker-rstudio:v2.2 .
+docker build -t haseong/rocker-rstudio:v3.0 .
 
--- rocker-rstudio/docker_run.sh
+++ rocker-rstudio/docker_run.sh
@@ -2,10 +2,10 @@
 docker run -d --rm \
 	-v /mnt/alpha:/mnt/alpha \
 	-v /etc/localtime:/etc/localtime \
-	-p 8888:8787 \
+	-p 8787:8787 \
 	--name rstudio \
-	haseong/rocker-rstudio:v2.2
+	haseong/rocker-rstudio:v3.0
 
 docker network connect sblab rstudio 
 
-sudo bash /mnt/alpha/dockerimages/bin/rstudio_users_sync.sh rstudio
+sudo bash /mnt/alpha/dockerimages/bin/docker_user_sync.sh rstudio
