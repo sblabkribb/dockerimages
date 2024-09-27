376958978ff8dfdfb2e2c17eb2c133cd0092848f
Haseong Kim <kimhaseong@gmail.com>
2024-09-27 17:31:02 +0900

    update mongodb

--- mongoDB/docker_run.sh
+++ mongoDB/docker_run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-docker run --rm -d -p 27027:27017 --name mongodb -v /mnt/alpha/mongodb:/data/db -v /mnt/alpha/share:/share haseong/mongodb:v6.0.3
+docker run --rm -d -p 27027:27017 --name mongodb -v /mnt/alpha/mongodb:/data/db -v /mnt/alpha/share:/share -e TZ=Asia/Seoul haseong/mongodb:v6.0.3 --auth
 #docker run --rm -d -p 3306:3306 --name mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=true -v /mnt/alpha/mysql:/var/lib/mysql mysql:$VER --skip-grant-tables
 
 #docker network connect sblab mongodb
